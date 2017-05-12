3acb545e745de42bbe4269b072e67ebe75791e85
FranciscoTavares <fco.anjos@gmail.com>
2017-05-12 12:32:57 -0300

    fouth commit

--- main.py
+++ main.py
@@ -7,3 +7,9 @@ instrumentos = ['Baixo','Bateria','Guitarra']
 for instrumento in instrumentos:
 	#mostre os nomes dos instrumentos musicais
 	print instrumento
+
+
+b=['a','b','c']
+c=range(1,5)
+print b,c
+
