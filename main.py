e9943f41d422dc0c1e9467691d1d10e2e2c2ffc6
FranciscoTavares <fco.anjos@gmail.com>
2017-05-12 11:04:39 -0300

    Third Commit

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-print "Hello world"
+#author Pythonist Francisco Tavares
 
+#uma lista de instrumentos musicais
+instrumentos = ['Baixo','Bateria','Guitarra']
+
+#para cada nome na lista de instrumentos
+for instrumento in instrumentos:
+	#mostre os nomes dos instrumentos musicais
+	print instrumento
